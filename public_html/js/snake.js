4b289a6c993c205f291b402ee401ba50f3a6c561
Erick Martinez <jclash626@gmail.com>
2014-10-08 09:15:06 -0700

    Changes
    On my way to adding a background color.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -4,6 +4,8 @@ var context;
 var screenWidth;
 var screenHeight;
 
+gameDraw();
+
 function gameIntialize() {
     var canvas = document.getElementById("game-screen");
     context = canvas.getContext("2d");
@@ -20,5 +22,6 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    
+    context.fillStyle = "rgb(228, 240, 7)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
 }
